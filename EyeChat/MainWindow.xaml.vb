9c0922e9e09536ac6e80bb8ba8c5a86a84da555d
benoit <the-gollum@hotmail.fr>
2023-05-12 12:26:31 +0200

    Ajoutez des fichiers projet.

---
+++ EyeChat/MainWindow.xaml.vb
@@ -0,0 +1,9 @@
+﻿Class MainWindow
+
+    Private Sub LaunchGitHubSite()
+        ' Exemple : Ouvrir le site GitHub dans le navigateur par défaut
+        Dim url As String = "https://github.com"
+        Process.Start(url)
+    End Sub
+
+End Class
